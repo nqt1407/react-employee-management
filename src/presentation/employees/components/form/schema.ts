2064326fd70bca79a3272bedc80da00f8ded9c55
nqt1407 <quangtnguyen1407@gmail.com>
2024-06-30 19:52:04 +0700

    wip: add eslint config

--- src/presentation/employees/components/form/schema.ts
+++ src/presentation/employees/components/form/schema.ts
@@ -1,12 +1,12 @@
-import { z, ZodIssueCode } from "zod";
-import { t } from "i18next";
+import { t } from 'i18next';
+import { z, ZodIssueCode } from 'zod';
 
 const toolsSchema = z.array(
   z
     .object({
       id: z.number().optional(),
       toolLanguageResourceId: z.number({
-        required_error: "Tool/Language is required",
+        required_error: 'Tool/Language is required',
       }),
       from: z.number().optional(),
       to: z.number().optional(),
@@ -14,12 +14,12 @@ const toolsSchema = z.array(
       images: z
         .any()
         .array()
-        .max(2, { message: "You can upload up to 2 images" })
+        .max(2, { message: 'You can upload up to 2 images' })
         .refine(
-          (files) => files.every((file) => file.type.startsWith("image/")),
+          (files) => files.every((file) => file.type.startsWith('image/')),
           {
-            message: "All files must be images",
-          }
+            message: 'All files must be images',
+          },
         )
         .optional(),
     })
@@ -29,37 +29,37 @@ const toolsSchema = z.array(
         if (from > to) {
           ctx.addIssue({
             code: ZodIssueCode.custom,
-            message: t("employee.form.input.toolExperienceFrom.afterToTime"),
-            path: ["from"],
+            message: t('employee.form.input.toolExperienceFrom.afterToTime'),
+            path: ['from'],
           });
         }
 
         if (to < from) {
           ctx.addIssue({
             code: ZodIssueCode.custom,
-            message: t("employee.form.input.toolExperienceTo.beforeFromTime"),
-            path: ["to"],
+            message: t('employee.form.input.toolExperienceTo.beforeFromTime'),
+            path: ['to'],
           });
         }
       }
 
       return z.NEVER;
-    })
+    }),
 );
 
 const positionsSchema = z.array(
   z.object({
     id: z.number().optional(),
     positionResourceId: z.number({
-      required_error: t("employee.form.input.position.required"),
+      required_error: t('employee.form.input.position.required'),
     }),
     toolLanguages: toolsSchema,
-  })
+  }),
 );
 
 export const employeeSchema = z.object({
   id: z.number().optional(),
-  name: z.string().min(1, t("employee.form.input.name.required")),
+  name: z.string().min(1, t('employee.form.input.name.required')),
   positions: positionsSchema,
 });
 
