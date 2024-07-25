c3e4b081afed4c949e471945ac5872fc7b8332e1
nqt1407 <quangtnguyen1407@gmail.com>
2024-07-25 11:07:02 +0700

    feat: add icon package

--- src/presentation/employees/components/list/Employees.tsx
+++ src/presentation/employees/components/list/Employees.tsx
@@ -1,3 +1,4 @@
+import { XCircleIcon } from '@heroicons/react/24/outline';
 import { useMemo, useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import InfiniteScroll from 'react-infinite-scroll-component';
@@ -54,20 +55,7 @@ const EmployeeCard = ({ employeeData, positionsMap }: EmployeeCardProps) => {
         aria-label={`Delete ${employeeData.name}`}
         data-testid="delete-btn"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-          />
-        </svg>
+        <XCircleIcon className="w-6 h-6" />
       </Button>
       {images.length > 0 && (
         <Swiper
--- src/presentation/employees/components/list/Search.tsx
+++ src/presentation/employees/components/list/Search.tsx
@@ -1,3 +1,4 @@
+import { UserPlusIcon } from '@heroicons/react/24/outline';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { z } from 'zod';
@@ -44,22 +45,7 @@ export const Search = ({ onSubmit }: SearchProps) => {
           size="sm"
           colorScheme="green"
           onClick={() => navigate('./new')}
-          startIcon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
-              />
-            </svg>
-          }
+          startIcon={<UserPlusIcon className="size-6" />}
         >
           {t('employee.list.button.addEmployee')}
         </Button>
