7bc8d9b8392c1f4972c1b15038ae9483d49a96c9
nqt1407 <quangtnguyen1407@gmail.com>
2024-09-23 23:44:20 +0700

    feat: Add tooltip

---
+++ src/components/overlay/tooltip/Tooltip.stories.tsx
@@ -0,0 +1,80 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from '../../forms/button';
+
+import { Tooltip, TooltipProps } from './Tooltip';
+
+const meta: Meta<TooltipProps> = {
+  title: 'Components/Overlay/Tooltip',
+  component: Tooltip,
+  parameters: {
+    layout: 'centered',
+    controls: { expanded: true },
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof Tooltip>;
+
+export const Default: Story = {
+  render: (args) => (
+    <div className="flex justify-center items-center h-screen">
+      <Tooltip {...args} label="Default Tooltip">
+        <Button className="px-4 py-2 bg-blue-500 text-white rounded-md">
+          Hover over me
+        </Button>
+      </Tooltip>
+    </div>
+  ),
+};
+
+export const TopDirection: Story = {
+  render: (args) => (
+    <div className="flex justify-center items-center h-screen">
+      <Tooltip {...args} label="Tooltip on top" direction="top">
+        <Button colorScheme="green">Hover me (Top)</Button>
+      </Tooltip>
+    </div>
+  ),
+};
+
+export const BottomDirection: Story = {
+  render: (args) => (
+    <div className="flex justify-center items-center h-screen">
+      <Tooltip {...args} label="Tooltip on bottom" direction="bottom">
+        <Button colorScheme="red">Hover me (Bottom)</Button>
+      </Tooltip>
+    </div>
+  ),
+};
+
+export const LeftDirection: Story = {
+  render: (args) => (
+    <div className="flex justify-center items-center h-screen">
+      <Tooltip {...args} label="Tooltip on left" direction="left">
+        <Button className=" bg-purple-500">Hover me (Left)</Button>
+      </Tooltip>
+    </div>
+  ),
+};
+
+export const RightDirection: Story = {
+  render: (args) => (
+    <div className="flex justify-center items-center h-screen">
+      <Tooltip {...args} label="Tooltip on right" direction="right">
+        <Button className="bg-orange-500">Hover me (Right)</Button>
+      </Tooltip>
+    </div>
+  ),
+};
+
+export const HasArrow: Story = {
+  render: (args) => (
+    <div className="flex justify-center items-center h-screen">
+      <Tooltip {...args} label="Tooltip with arrow" direction="top" hasArrow>
+        <Button>Hover me</Button>
+      </Tooltip>
+    </div>
+  ),
+};
---
+++ src/components/overlay/tooltip/Tooltip.tsx
@@ -0,0 +1,96 @@
+import { Transition } from '@headlessui/react';
+import clsx from 'clsx';
+import React, { useState } from 'react';
+
+type Direction =
+  | 'top'
+  | 'top-start'
+  | 'top-end'
+  | 'bottom'
+  | 'bottom-start'
+  | 'bottom-end'
+  | 'left'
+  | 'left-start'
+  | 'left-end'
+  | 'right'
+  | 'right-start'
+  | 'right-end';
+
+const tooltipPositions: Record<Direction, string> = {
+  top: 'bottom-full left-1/2 transform -translate-x-1/2 mb-2',
+  'top-start': 'bottom-full left-0 mb-2',
+  'top-end': 'bottom-full right-0 mb-2',
+  bottom: 'top-full left-1/2 transform -translate-x-1/2 mt-2',
+  'bottom-start': 'top-full left-0 mt-2',
+  'bottom-end': 'top-full right-0 mt-2',
+  left: 'right-full top-1/2 transform -translate-y-1/2 mr-2',
+  'left-start': 'right-full top-0 mr-2',
+  'left-end': 'right-full bottom-0 mr-2',
+  right: 'left-full top-1/2 transform -translate-y-1/2 ml-2',
+  'right-start': 'left-full top-0 ml-2',
+  'right-end': 'left-full bottom-0 ml-2',
+};
+
+const arrowPositions: Record<Direction, string> = {
+  top: 'bottom-0 left-1/2 transform -translate-x-1/2 translate-y-full rotate-45',
+  'top-start': 'bottom-0 left-2 translate-y-full rotate-45',
+  'top-end': 'bottom-0 right-2 translate-y-full rotate-45',
+  bottom:
+    'top-0 left-1/2 transform -translate-x-1/2 -translate-y-full rotate-45',
+  'bottom-start': 'top-0 left-2 -translate-y-full rotate-45',
+  'bottom-end': 'top-0 right-2 -translate-y-full rotate-45',
+  left: 'right-0 top-1/2 transform -translate-y-1/2 translate-x-full rotate-45',
+  'left-start': 'right-0 top-2 translate-x-full rotate-45',
+  'left-end': 'right-0 bottom-2 translate-x-full rotate-45',
+  right:
+    'left-0 top-1/2 transform -translate-y-1/2 -translate-x-full rotate-45',
+  'right-start': 'left-0 top-2 -translate-x-full rotate-45',
+  'right-end': 'left-0 bottom-2 -translate-x-full rotate-45',
+};
+
+export type TooltipProps = {
+  label: string;
+  children: React.ReactNode;
+  hasArrow?: boolean;
+  direction?: Direction;
+};
+
+export const Tooltip: React.FC<TooltipProps> = ({
+  label,
+  children,
+  hasArrow = false,
+  direction = 'top',
+}) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div className="relative inline-block">
+      <div
+        onMouseEnter={() => setIsVisible(true)}
+        onMouseLeave={() => setIsVisible(false)}
+        className="cursor-pointer"
+      >
+        {children}
+      </div>
+      <Transition show={isVisible}>
+        <div
+          className={clsx(
+            'absolute w-max bg-gray-700 text-white text-sm rounded-lg px-2 py-1 shadow-lg z-10',
+            'transition duration-100 ease-in data-[closed]:opacity-0',
+            tooltipPositions[direction],
+          )}
+        >
+          {label}
+          {hasArrow && (
+            <div
+              className={clsx(
+                'absolute w-2 h-2 bg-gray-700 origin-center rotate-45 bottom-1',
+                arrowPositions[direction],
+              )}
+            />
+          )}
+        </div>
+      </Transition>
+    </div>
+  );
+};
